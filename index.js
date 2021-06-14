5e907f071b6633ea724d79aa118dcc9238687025
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-14 19:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ class Circle{
   get diameter(){
     return this.radius * 2;
   }
+  
+  get circumference(){
+    return this.radius * 2 * Math.PI();
+  }
+  
+  get area(){
+    return this.radius * this.radius * Math.PI();
+  }
+  
+  set diameter(value){
+    return this.radius = value/2;
+  }
+  
+  set circumference(value){
+    return this.radius = value/(this.radius * Math.PI();)
+  }
+  
+  set area(value){
+    return this.radius * 2;
+  }
 }
